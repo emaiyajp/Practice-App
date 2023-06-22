3b91faf9e192143fe1c96954c095fbbc75b238ff
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-22 10:41:51 -0600

    Initial Commit

---
+++ Practice App/Practice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_AppApp.swift
+//  Practice App
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Practice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
